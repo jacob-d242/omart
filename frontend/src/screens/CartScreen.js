0b768ec6c83cf1ccb79d55ceca113578d8cfa6ff
jacob-d242 <jacobmoracha@gmail.com>
2021-11-12 15:39:59 +0300

    make it better

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -86,7 +86,7 @@ export default function CartScreen(props) {
           <ul>
             <li>
               <h2>
-                Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : $
+                Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : KSh
                 {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
               </h2>
             </li>
